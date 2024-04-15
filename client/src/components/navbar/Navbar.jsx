2fd956ced7923637af09619a55c2e76fd1cfb0d6
brady <bradybill36@gmail.com>
2024-04-15 16:01:15 -0400

    fixed a bunch of styling with the navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -5,35 +5,37 @@ import './navbar.css'
 function Navbar() {
   return (
     <header>
-        <nav>
-            <div className='logo-container'>
-                <Link className='logo-container' to='/'>
-                    <img className='nav-logo' src={bradyLogo} alt="Brady Logo" />
-                    <h1>Brady Billeisen</h1>
-                </Link>
-            </div>
-            <ul className='nav-items'>
-                <li>
-                    <Link className='nav-links' to={'/'}>Home</Link>
-                </li>
-                <li>
-                    <Link className='nav-links' to={'/about'}>About</Link>
-                </li>
-                <li>
-                    <Link className='nav-links' to={'/contact'}>Contact</Link>
-                </li>
-                <li>
-                    <Link className='nav-links' to={'/resume'}>Resume</Link>
-                </li>
-            </ul>
-            <ul className='portfolio-btn-container'>
-                <Link className='portfolio-item' to={'/portfolio'}>
+        <div className='nav-flex'>
+            <nav className='nav-container'>
+                <div className='logo-container'>
+                    <Link className='logo-container' to='/'>
+                        <img className='nav-logo' src={bradyLogo} alt="Brady Logo" />
+                        <h1>Brady Billeisen</h1>
+                    </Link>
+                </div>
+                <ul className='nav-items'>
                     <li>
-                        <button className='portfolio-btn'>Portfolio</button>
+                        <Link className='nav-links' to={'/'}>Home</Link>
                     </li>
-                </Link>
-            </ul>
-        </nav>
+                    <li>
+                        <Link className='nav-links' to={'/about'}>About</Link>
+                    </li>
+                    <li>
+                        <Link className='nav-links' to={'/contact'}>Contact</Link>
+                    </li>
+                    <li>
+                        <Link className='nav-links' to={'/resume'}>Resume</Link>
+                    </li>
+                </ul>
+                <ul className='portfolio-btn-container'>
+                    <Link className='portfolio-item' to={'/portfolio'}>
+                        <li>
+                            <button className='portfolio-btn'>Portfolio</button>
+                        </li>
+                    </Link>
+                </ul>
+            </nav>
+        </div>
     </header>
   )
 }
